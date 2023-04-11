e66597464313ca6f9b24f5774a6b06ee066e8cf3
Kandreas9 <andkarido@gmail.com>
2023-04-11 10:29:35 +0200

    find books action

--- cli.py
+++ cli.py
@@ -1,3 +1,5 @@
+import json
+
 class Cli:
     def __init__(self):
         print('===Welcome To The Library System===')
@@ -19,7 +21,29 @@ Bienvenue que désirez vous faire ?
         action = input()
 
         match action:
+            case '1':
+                self.findBook()
+                return True
             case '6':
                 return False
             case _:
                 return True
+
+    def findBook(self):
+        print('Quel livre cherchez vous ?')
+
+        print('===>', end=" ")
+        bookName = input()
+
+        file = open('livres.json')
+        books = json.load(file)
+
+        for index, book in enumerate(books):
+            if bookName in book['titre']:
+                disponible = 'Disponible' if book['disponible'] else 'Non Disponible'
+
+                print('Livre correspondant à la recherche :')
+                print(f"nº {index} {book['titre']}: {disponible}")
+
+
+        
